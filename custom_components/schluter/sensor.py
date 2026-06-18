5353372fba7bad694fe413d3009bb068d7785314
Mike Adler <aepitaz@users.noreply.github.com>
2026-06-18 16:16:16 -0400

    Update sensor.py
    Using Total to remove all syntax errors

--- custom_components/schluter/sensor.py
+++ custom_components/schluter/sensor.py
@@ -201,7 +201,7 @@ class SchluterEnergyPriceSensor(SchluterEntity, SensorEntity):
 
     _attr_native_unit_of_measurement = "$/kWh"
     _attr_device_class = SensorDeviceClass.MONETARY
-    _attr_state_class = SensorStateClass.MEASUREMENT
+    _attr_state_class = SensorStateClass.TOTAL
 
     def __init__(
         self,
